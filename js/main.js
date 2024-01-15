8a006501469a273e7a171f41c108570f67f5ca6e
Юлия Козлова <75185563+kozlova-ut@users.noreply.github.com>
2024-01-15 23:21:01 +0300

    popup added

--- js/main.js
+++ js/main.js
@@ -24,3 +24,56 @@ for (let selector of selectors) {
         }
     })
 }
+
+
+const buttons = document.querySelectorAll('.button');
+const popup = document.querySelector('.popup-wrapper');
+const close = document.querySelector('.close');
+const heading = popup.querySelector('.heading');
+const form = popup.querySelector('form');
+const success = popup.querySelector('.success');
+
+for (let button of buttons) {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popup.style.display = 'flex';
+        heading.style.display = 'block';
+        form.style.display = 'flex';
+        success.style.display = 'none';
+    })
+}
+
+close.addEventListener('click', () => {
+    popup.style.display = 'none';
+})
+
+const TOKEN = "5443287425:AAEj0MA2OYh2GShTdzuKSrttCK2QBuDHUvQ";
+const CHAT_ID = "-1001682187999";
+const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
+document.getElementById('tg').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    let message = `<b>Сообщение с сайта</b>\n`;
+
+    message += `<b>Почта:</b> ${this.email.value}\n`;
+    message += `<b>Телефон:</b> ${this.phone.value}\n`;
+    message += `<b>Имя:</b> ${this.name.value}`;
+
+    axios.post(URI_API, {
+        chat_id: CHAT_ID,
+        parse_mode: 'html',
+        text: message
+    })
+    .then((res) => {
+        heading.style.display = 'none';
+        form.style.display = 'none';
+        success.style.display = 'block';
+    })
+    .catch((err) => {
+        console.warn(err);
+    })
+    .finally(() => {
+        console.log('Сообщение отправлено');
+    })
+})
